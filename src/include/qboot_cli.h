336ecdd3ec6f8e62e7a9da9aeb72e6170b042352
föllmy <foellmy@foellmy.de>
2026-09-23 12:18:52 +0200

    feat: add main boot flow, interactive shell, and bare-metal library

---
+++ src/include/qboot_cli.h
@@ -0,0 +1,10 @@
+#ifndef QBOOT_CLI_H
+#define QBOOT_CLI_H
+
+/**
+ * @brief Startet die interaktive Bootloader-Shell.
+ * Diese läuft in einer Eingabeschleife auf dem Terminal.
+ */
+void cli_run(void);
+
+#endif /* QBOOT_CLI_H */
